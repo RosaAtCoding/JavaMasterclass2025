e9d1cc079d7ddf4fecfc9f058c696364f8d84161
Rosa Mø <rosaatcoding@gmail.com>
2026-07-13 23:02:59 +0200

    36.Classes, Methods, and Basic Java Syntax

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,7 +1,9 @@
-public class FirstClass {
+public class Hello {
 
     public static void main(String[] args) {
 
         System.out.println("Hello, Thor");
+
+        System.out.print("Hello World");
     }
 }
